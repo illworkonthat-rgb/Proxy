9c8805a51062918947e8606d549c9ad30e7352f9
illworkonthat-rgb <illworkonthat@gmail.com>
2026-05-13 21:48:31 -0500

    Update uv.sw.js

--- public/uv/uv.sw.js
+++ public/uv/uv.sw.js
@@ -1,1 +1,10 @@
+/* eslint-disable no-undef */
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts('/uv/uv.sw.js'); // This refers to the internal logic within the bundle
 
+const sw = new UVServiceWorker();
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(sw.fetch(event));
+});
